786247d11916792dd91c917012114a0b04000cee
AmiT-Gupta7 <guptaamit61739@gmail.com>
2026-03-18 23:39:57 +0530

    Sort

---
+++ Containers/Sort.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+exampleSort(){
+
+    sort(a, a+n);
+    // sort(v.begin(), v.end());
+
+    sort(a+2, a+4);
+
+    pair<int,int> a[] = {{1, 2}, {3, 4}, {1, 1}};
+
+    
+}
+
+int main() {
+    exampleSort();
+    return 0;
+}
