480b987c1927dd540010fd21cf5f036de708b10b
AmiT-Gupta7 <guptaamit61739@gmail.com>
2026-03-23 22:20:27 +0530

    recursion

---
+++ Recursion/String_Palindrome.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+bool strPalindrome(int i, string &s){
+    if(i >= s.size() / 2) return true;
+    if(s[i] != s[s.size()-i-1]){
+        return false;
+    }
+    return strPalindrome(i+1, s);
+}
+
+
+int main(){
+    string s = "";
+    cout << "Enter String:";
+    cin >> s;
+    cout << strPalindrome(0, s);
+    return 0;
+}
