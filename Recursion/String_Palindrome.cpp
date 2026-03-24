284e835ec9d343179ce0f8fca999afc49837a685
AmiT-Gupta7 <guptaamit61739@gmail.com>
2026-03-24 23:53:21 +0530

    recursion

--- Recursion/String_Palindrome.cpp
+++ Recursion/String_Palindrome.cpp
@@ -9,11 +9,43 @@ bool strPalindrome(int i, string &s){
     return strPalindrome(i+1, s);
 }
 
+// bool isPalindrome(string s){
+//     string newstr = "";
+//     for(int i = 0; i < s.size(); i++){
+//         if(isalpha(s[i])){
+//             newstr.push_back(tolower(s[i]));
+//         }
+//     }
+//     string reveseStr = newstr;
+
+//     reverse(reveseStr.begin(), reveseStr.end());
+
+//     return reveseStr == newstr;
+// }
+bool isPalindrome2(string s) {
+    int left = 0, right = s.size() - 1;
+
+    while (left < right) {
+        if (!isalpha(s[left])) {
+            left++;
+        } else if (!isalpha(s[right])) {
+            right--;
+        } else {
+            if (tolower(s[left]) != tolower(s[right])) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+    }
+    return true;
+}
 
 int main(){
     string s = "";
     cout << "Enter String:";
     cin >> s;
-    cout << strPalindrome(0, s);
-    return 0;
+    // cout << strPalindrome(0, s);
+    // return 0;
+    cout << isPalindrome2(s);
 }
